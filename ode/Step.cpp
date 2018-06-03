061389d06e84b8d6e08c8fdca2617c46b38bb909
Tom S <stumpf.tn@gmail.com>
2018-06-03 07:46:50 -0500

    commit

--- ode/Step.cpp
+++ ode/Step.cpp
@@ -4,9 +4,68 @@
 
 Step::Step()
 {
+	double u = machine();
+	twou = 2 * u;
+	fouru = 4 * u;
 }
 
 
 Step::~Step()
 {
+}
+
+void Step::block0()
+{
+	crash = false;
+	test_inputs();
+	if (crash) { return; }
+	if (start) { initialize(); }
+
+}
+
+char Step::sign(double a)
+{
+	int b = a > 0 ? 1 : -1;
+	return b;
+}
+
+double Step::machine()
+{
+	double halfu = 0.5;
+	while (1 + halfu > 1)
+	{
+		halfu *= 0.5;
+	}
+	double u = 2 * halfu;
+	return u;
+}
+
+void Step::test_inputs()
+{
+	if (abs(h) < fouru * abs(x))
+	{
+		h = fouru * abs(x) * sign(h);
+		crash = true;
+		return;
+	}
+	
+	double round = 0.0;
+	for (size_t i = 0; i < neqn; i++)
+	{
+		round += pow(y[i] / wt[i], 2);
+	}
+	round = twou * sqrt(round);
+
+	if (0.5 * eps < round)
+	{
+		eps = 2.0 * round * (1 + fouru);
+		crash = true;
+		return;
+	}
+}
+
+void Step::initialize()
+{
+	f(x, y, yp);
+
 }
--- ode/Step.h
+++ ode/Step.h
@@ -1,8 +1,54 @@
 #pragma once
+#include <cmath>
 class Step
 {
 public:
+	// constants
+
+	// constructor and destructor
 	Step();
 	~Step();
+
+	// logical
+	bool crash;
+	bool start;
+
+
+	// variables
+	unsigned char neqn = 1;
+
+	double twou;
+	double fouru;
+	double eps;
+
+	double h;
+	double x;
+
+	
+
+	// arrays
+	double y[1];
+	double yp[1];
+	double wt[1];
+
+	// member functions
+	void(*f)(double, double[], double[]);
+
+	char sign(double);
+	double machine();
+	
+	void block0();
+	//void block1();
+	//void block2();
+	//void block3();
+	//void block4();
+
+	void test_inputs();
+	void initialize();
+	//void compute_coefficients();
+	//void initialize_vw();
+	//void update_vw();
+	//void compute_g();
+
 };
 
